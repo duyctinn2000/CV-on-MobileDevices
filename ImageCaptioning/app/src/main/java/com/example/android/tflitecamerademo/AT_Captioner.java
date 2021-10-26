08cc44f19b5bd66dc250f577316dabb038e208cb
Hoang Duy <duyctin2000@gmail.com>
2021-10-26 22:15:40 +0700

    add ImageCaptionning

---
+++ ImageCaptioning/app/src/main/java/com/example/android/tflitecamerademo/AT_Captioner.java
@@ -0,0 +1,407 @@
+package com.example.android.tflitecamerademo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.os.SystemClock;
+import android.util.Log;
+
+import org.tensorflow.lite.gpu.CompatibilityList;
+import org.tensorflow.lite.gpu.GpuDelegate;
+
+
+import org.tensorflow.lite.Interpreter;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class AT_Captioner {
+
+    private static final String TAG = "TfLiteCameraDemo";
+    private final Interpreter.Options tfliteOptions_encoder = (new Interpreter.Options());
+    private final Interpreter.Options tfliteOptions_decoder = (new Interpreter.Options());
+    private final Interpreter.Options tfliteOptions_cnn = (new Interpreter.Options());
+
+    // The loaded TensorFlow Lite model.
+    private MappedByteBuffer tfliteModel_encoder;
+    private MappedByteBuffer tfliteModel_decoder;
+    private MappedByteBuffer tfliteModel_cnn;
+
+    // An instance of the driver class to run model inference with Tensorflow Lite.
+    protected Interpreter tflite_encoder;
+    protected Interpreter tflite_decoder, tflite_cnn;
+    // A ByteBuffer to hold image data, to be feed into Tensorflow Lite as inputs.
+    private int [][] input_feed = null;
+    private float [][] hiden_feed = null;
+    private float [][][] input_encoder = null;
+    private float [][][][] image_feed = null;
+    private float [][][] decoder_feed = null;
+    private float [][][][] output_cnn = null;
+    private float [][] decoder_predict = null;
+    private float [][] decoder_hidden = null;
+    private float [][][] decoder_attention = null;
+    // Float model
+    private final float IMAGE_MEAN = 127.5f; //0;
+
+    private final float IMAGE_STD = 127.5f; //255.0f;
+    private final int IMAGE_SIZE = 299;
+    private ByteBuffer imgData;
+    private int[] intValues;
+    private Activity ac;
+
+    private static final int NUM_THREADS = 2;
+
+
+    Vocab vocab;
+
+    AT_Captioner(Activity activity) throws IOException {
+
+        String encoderModelPath = getEncoderModelPath();
+        String decoderModelPath = getDecoderModelPath();
+        String cnnModelPath = getCNNModelPath();
+        ac = activity;
+
+        tfliteOptions_cnn.setNumThreads(NUM_THREADS);
+        tfliteModel_cnn = loadModelFile(activity, cnnModelPath);
+        tflite_cnn = new Interpreter(tfliteModel_cnn, tfliteOptions_cnn);
+
+        tfliteOptions_encoder.setNumThreads(NUM_THREADS);
+        tfliteModel_encoder = loadModelFile(activity, encoderModelPath);
+        tflite_encoder = new Interpreter(tfliteModel_encoder, tfliteOptions_encoder);
+
+        tfliteOptions_decoder.setNumThreads(NUM_THREADS);
+        tfliteModel_decoder = loadModelFile(activity, decoderModelPath);
+        tflite_decoder = new Interpreter(tfliteModel_decoder, tfliteOptions_decoder);
+
+        Log.d(TAG, "Created a Tensorflow Lite Image Classifier.");
+
+        output_cnn = new float[1][8][8][2048];
+
+        input_encoder = new float[1][64][2048];
+
+        decoder_feed = new float[1][64][256];
+        input_feed = new int[1][1];
+        hiden_feed = new float[1][512];
+
+        decoder_predict = new float[1][4369];//[9196];
+        decoder_hidden = new float[1][512];
+        decoder_attention = new float[1][64][1];
+        imgData = ByteBuffer.allocateDirect(1 * IMAGE_SIZE * IMAGE_SIZE * 3 * 4);
+        imgData.order(ByteOrder.nativeOrder());
+        intValues = new int[IMAGE_SIZE * IMAGE_SIZE];
+        image_feed = new float[1][299][299][3];
+        //loadImage(activity);
+        vocab = new Vocab(activity);
+
+    }
+
+    // Classifies a frame from the preview stream.
+    String classifyFrame() {
+        if (tflite_cnn == null) {
+            Log.e(TAG, "Image classifier has not been initialized; Skipped.");
+            //builder.append(new SpannableString("Uninitialized Classifier."));
+        }
+        //convertBitmapToByteBuffer(bitmap);
+        // Here's where the magic happens!!!
+        long startTime = SystemClock.uptimeMillis();
+        String caption = runInference();
+        long endTime = SystemClock.uptimeMillis();
+        Log.d(TAG, "Timecost to run model inference: " + Long.toString(endTime - startTime));
+
+        // Print the results.
+        long duration = endTime - startTime;
+
+        return caption + "\t" + String.valueOf((float) duration/1000);
+    }
+
+    // Closes tflite to release resources.
+    public void close() {
+        tflite_decoder.close();
+        tflite_decoder = null;
+        tfliteModel_decoder = null;
+
+        tflite_encoder.close();
+        tflite_encoder = null;
+        tfliteModel_encoder = null;
+
+        tflite_cnn.close();
+        tflite_cnn = null;
+        tfliteModel_cnn = null;
+
+    }
+
+
+    protected ByteBuffer convertBitmapToByteBuffer(Bitmap bitmap) throws IOException {
+        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        String output = "";
+        imgData.rewind();
+        Log.i("123123",ac.getExternalFilesDir("MyFileDir").toString());
+        File myExternalFile = new File(ac.getExternalFilesDir("MyFileDir"),"image.txt");
+        FileOutputStream fos = null;
+        fos = new FileOutputStream(myExternalFile);
+        for (int i = 0; i < IMAGE_SIZE; ++i) {
+            for (int j = 0; j < IMAGE_SIZE; ++j) {
+                int pixelValue = intValues[i * IMAGE_SIZE + j];
+                float x = (((pixelValue >> 16) & 0xFF) - IMAGE_MEAN) / IMAGE_STD;
+                float y = (((pixelValue >> 8) & 0xFF) - IMAGE_MEAN) / IMAGE_STD;
+                float z = ((pixelValue & 0xFF) - IMAGE_MEAN) / IMAGE_STD;
+                imgData.putFloat((((pixelValue >> 16) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+                imgData.putFloat((((pixelValue >> 8) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+                imgData.putFloat(((pixelValue & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+                output =  String.valueOf(x) + " " + String.valueOf(y) + " " + String.valueOf(z) + "\n";
+                fos.write(output.getBytes());
+                //Log.i("12321",String.valueOf(x) + " " + String.valueOf(y) + " " + String.valueOf(z));
+            }
+            Log.i("1231231",String.valueOf(i));
+        }
+        fos.close();
+        //Log.i("1231",output);
+        return imgData;
+    }
+
+    // Memory-map the model file in Assets.
+    private MappedByteBuffer loadModelFile(Activity activity, String modelFileName) throws IOException {
+        AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(modelFileName);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+
+    private String getCNNModelPath() {
+        return "cnn_model_v4.tflite";
+    }
+
+    private String getEncoderModelPath() {
+        return "encoder_model_v4.tflite";
+    }
+
+    private String getDecoderModelPath() {
+        return "decoder_model_v4.tflite";
+    }
+
+    private float[][][] reshape(float [][][][] output_cnn){
+        float[][][] input_encoder = new float[1][64][2048];
+        for (int x = 0; x<1; x++) {
+            for (int y = 0; y<8; y++) {
+                for (int z = 0; z<8; z++) {
+                    for (int t = 0; t<2048; t++) {
+                        input_encoder[x][(y + 1) * (z + 1) - 1][t] = output_cnn[x][y][z][t];
+                    }
+                }
+            }
+        }
+        return input_encoder;
+    }
+
+    public void loadImage(Activity activity) throws IOException {
+
+
+        BufferedReader reader = null;
+
+        try {
+            // use buffered reader to read line by line
+            reader = new BufferedReader(new InputStreamReader(activity.getAssets().open("test.txt")));
+
+            String line = null, output="";
+            String[] numbers = null;
+            // read line by line till end of file
+            while ((line = reader.readLine()) != null) {
+                // split each line based on regular expression having
+                // "any digit followed by one or more spaces".
+
+                numbers = line.split(" ");
+
+                imgData.putFloat(Float.valueOf(numbers[0].trim()));
+                imgData.putFloat(Float.valueOf(numbers[1].trim()));
+                imgData.putFloat(Float.valueOf(numbers[2].trim()));
+                float x = Float.valueOf(numbers[0].trim());
+                float y = Float.valueOf(numbers[1].trim());
+                float z = Float.valueOf(numbers[2].trim());
+                output =  String.valueOf(x) + " " + String.valueOf(y) + " " + String.valueOf(z) + "\n";
+                Log.i("123123",output);
+            }
+        } catch (IOException e) {
+            System.err.println("Exception:" + e.toString());
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.err.println("Exception:" + e.toString());
+                }
+            }
+        }
+
+
+    }
+
+
+    private String runInference(){
+
+
+        tflite_cnn.run(imgData,output_cnn);
+        input_encoder = this.reshape(output_cnn);
+        tflite_encoder.run(input_encoder,decoder_feed);
+
+        int maxCaptionLength = 20;
+        List<Integer> words = new ArrayList<Integer>();
+
+        words.add(vocab.getStartIndex());
+
+        for(int i=0;i<hiden_feed[0].length;i++)
+        {
+            hiden_feed[0][i] = 1;
+        }
+
+        input_feed[0][0] = words.get(words.size() - 1);
+
+        Object [] inputs_decoder = new Object[3];
+        inputs_decoder[0] = hiden_feed;
+        inputs_decoder[1] = input_feed;
+        inputs_decoder[2] = decoder_feed;
+
+        Map<Integer, Object> outputs_decoder = new TreeMap<Integer, Object>();
+        outputs_decoder.put(0,decoder_predict);
+        outputs_decoder.put(1,decoder_attention);
+        outputs_decoder.put(2,decoder_hidden);
+
+        int maxIdx;
+
+        for(int i=0;i<maxCaptionLength;i++)
+        {
+            tflite_decoder.runForMultipleInputsOutputs(inputs_decoder, outputs_decoder);
+            maxIdx = findMaximumIdx(decoder_predict[0]);
+            words.add(maxIdx);
+            // TODO - replace with vocab.end_id
+            if(maxIdx == vocab.getEndIndex())
+            {
+                break;
+            }
+
+            input_feed[0][0] = (int)maxIdx;
+            for(int j=0;j<hiden_feed[0].length;j++)
+            {
+                hiden_feed[0][j] = decoder_hidden[0][j];
+            }
+            Log.d(TAG, "current word: " + maxIdx);
+
+        }
+
+        Log.d(TAG, "run inference complete");
+        //Log.d(TAG, "result= " + Arrays.deepToString(initial_state));
+        StringBuilder sb = new StringBuilder();
+        for(int i=1;i<words.size()-1;i++)
+        {
+            sb.append(vocab.getWordAtIndex(words.get(i)));
+            sb.append(" ");
+        }
+
+        return sb.toString();
+
+    }
+
+
+    private int findMaximumIdx(float [] arr)
+    {
+        int n = arr.length;
+        float maxval = arr[0];
+        int maxidx = 0;
+        for(int i=1;i<n;i++)
+        {
+            if(maxval < arr[i])
+            {
+                maxval = arr[i];
+                maxidx = i;
+            }
+        }
+        return maxidx;
+    }
+
+    public String predictImage(Bitmap origImg) {
+        Bitmap resized_img = Utils.processBitmap(origImg, IMAGE_SIZE);
+        try {
+            imgData = convertBitmapToByteBuffer(resized_img);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return classifyFrame();
+    }
+
+    private static byte[] stringToBytes(String s) {
+        return s.getBytes(StandardCharsets.US_ASCII);
+    }
+}
+
+
+class Vocab
+{
+
+    List<String> id2word;
+    Vocab(Activity activity)
+    {
+        try
+        {
+            id2word = loadWords(activity);
+        }
+        catch(Exception e)
+        {
+
+        }
+
+    }
+
+    public String getLabelPath()
+    {
+        return "words_v4.txt";
+    }
+
+    public List<String> loadWords(Activity activity) throws IOException {
+        List<String> labelList = new ArrayList<String>();
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(activity.getAssets().open(getLabelPath())));
+        String line;
+        String [] parts;
+        while ((line = reader.readLine()) != null) {
+            parts = line.split(" ");
+            labelList.add(parts[0]);
+        }
+        reader.close();
+        return labelList;
+    }
+
+    public int getStartIndex()
+    {
+        return 3;
+    }
+
+    public int getEndIndex() { return 4; }
+
+    public String getWordAtIndex(int index)
+    {
+        return id2word.get(index);
+    }
+
+}
---
+++ ImageCaptioning/app/src/main/java/com/example/android/tflitecamerademo/MainActivity.java
@@ -0,0 +1,285 @@
+package com.example.android.tflitecamerademo;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.AssetManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.speech.tts.TextToSpeech;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_IMAGE_CAPTURE = 100;
+    private static final int REQUEST_IMAGE_SELECT = 200;
+    private static final String IMAGE_DOG = "image.jpg";
+    private static final String fileName = "output.jpg";
+    private Button btnCamera;
+    private Button btnSelect, btnRotateLeft;
+    private Button btnPlay;
+    private ImageView ivCaptured;
+    private TextView tvLabel, timeLabel;
+    private Bitmap bmp;
+    private AT_Captioner captioner;
+    private Captioner lstm_captioner;
+    private TextToSpeech tts;
+    private String imgPath;
+    private File mFile;
+
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission1 = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission1 != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        verifyStoragePermissions(this);
+        setContentView(R.layout.activity_main);
+        tts = new TextToSpeech(this.getApplicationContext(), new TextToSpeech.OnInitListener()
+        {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.US);
+                    tts.setPitch(1.3f);
+                    tts.setSpeechRate(1f);
+                }
+            }
+        });
+
+        mFile = getPhotoFile();
+        final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        bmp = getBitmapFromAsset(MainActivity.this, IMAGE_DOG);
+
+        ivCaptured = (ImageView) findViewById(R.id.ivCaptured);
+        ivCaptured.setImageBitmap(resize(bmp));
+        tvLabel = (TextView) this.findViewById(R.id.tvlabel);
+        timeLabel = (TextView) findViewById(R.id.infereceTime);
+
+        btnRotateLeft = (Button) findViewById(R.id.id_btnRotateLeft);
+        btnRotateLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bmp != null) {
+                    Matrix matrix = new Matrix();
+                    matrix.postRotate(90);
+                    Bitmap rotatedBitmap = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, true);
+                    bmp = rotatedBitmap;
+                    ivCaptured.setImageBitmap(resize(bmp));
+                }
+            }
+        });
+
+        btnPlay = (Button) this.findViewById(R.id.id_btnPlay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String attention_result = captioner.predictImage(bmp);
+                //String lstm_result = lstm_captioner.predictImage(bmp);
+                onTaskCompleted("attention_result",attention_result);
+            }
+        });
+
+
+        btnCamera = (Button) this.findViewById(R.id.id_btnCamera);
+        btnCamera.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                initPrediction();
+                Uri uri = FileProvider.getUriForFile(MainActivity.this,
+                        "com.example.android.tflitecamerademo.fileprovider",
+                        mFile);
+                captureImage.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                List<ResolveInfo> cameraActivities = getPackageManager().queryIntentActivities(captureImage,
+                                PackageManager.MATCH_DEFAULT_ONLY);
+                for (ResolveInfo activity : cameraActivities) {
+                    grantUriPermission(activity.activityInfo.packageName,uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                }
+                startActivityForResult(captureImage, REQUEST_IMAGE_CAPTURE);
+            }
+        });
+
+        btnSelect = (Button) this.findViewById(R.id.id_btnSelect);
+        btnSelect.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                initPrediction();
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, REQUEST_IMAGE_SELECT);
+            }
+        });
+
+        try
+        {
+            captioner = new AT_Captioner(this);
+            lstm_captioner = new Captioner(this);
+        }
+        catch (Exception e)
+        {
+
+        }
+
+    }
+
+    private static Bitmap resize(Bitmap image) {
+        int maxHeight = 2000;
+        int maxWidth = 2000;
+        if (maxHeight > 0 && maxWidth > 0) {
+            int width = image.getWidth();
+            int height = image.getHeight();
+            float ratioBitmap = (float) width / (float) height;
+            float ratioMax = (float) maxWidth / (float) maxHeight;
+
+            int finalWidth = maxWidth;
+            int finalHeight = maxHeight;
+            if (ratioMax > ratioBitmap) {
+                finalWidth = (int) ((float)maxHeight * ratioBitmap);
+            } else {
+                finalHeight = (int) ((float)maxWidth / ratioBitmap);
+            }
+            image = Bitmap.createScaledBitmap(image, finalWidth, finalHeight, true);
+            return image;
+        } else {
+            return image;
+        }
+    }
+
+    public static Bitmap getBitmapFromAsset(Context context, String filePath) {
+        AssetManager assetManager = context.getAssets();
+
+        InputStream istr;
+        Bitmap bitmap = null;
+        try {
+            istr = assetManager.open(filePath);
+            bitmap = BitmapFactory.decodeStream(istr);
+//            return bitmap.copy(Bitmap.Config.ARGB_8888,true);
+        } catch (IOException e) {
+            // handle exception
+            Log.e("getBitmapFromAsset", "getBitmapFromAsset: " + e.getMessage());
+        }
+
+        return bitmap;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if ((requestCode == REQUEST_IMAGE_CAPTURE || requestCode == REQUEST_IMAGE_SELECT) && resultCode == RESULT_OK) {
+
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                imgPath = mFile.getPath();
+            } else {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                Cursor cursor = MainActivity.this.getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imgPath = cursor.getString(columnIndex);
+                cursor.close();
+            }
+
+            bmp = BitmapFactory.decodeFile(imgPath);
+
+            ivCaptured.setImageBitmap(resize(bmp));
+
+        } else {
+            btnCamera.setEnabled(true);
+            btnSelect.setEnabled(true);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void initPrediction() {
+//        btnCamera.setEnabled(false);
+//        btnSelect.setEnabled(false);
+        tvLabel.setText("");
+        timeLabel.setText("");
+        tvLabel.setBackgroundColor(Color.WHITE);
+    }
+
+    private void speak_out() {
+        String caption=tvLabel.getText().toString();
+        tts.speak(caption, TextToSpeech.QUEUE_ADD, null,null);
+    }
+    /**
+     * display the results on screen
+     */
+    public void onTaskCompleted(String attention_result, String lstm_result) {
+//        String inferTime = "Attention: " + attention_result.split("\t")[1] + "\nLSTM: " + lstm_result.split("\t")[1];
+//        String caption = "Attention: "+attention_result.split("\t")[0] + "\nLSTM: "+lstm_result.split("\t")[0];
+        String inferTime = lstm_result.split("\t")[1];
+        String caption = lstm_result.split("\t")[0];
+//        tvLabel.setText(caption);
+//        timeLabel.setText(inferTime);
+
+        tvLabel.setText("Caption: " + caption);
+        timeLabel.setText("Inference Time: "+inferTime+"s");
+        tvLabel.setBackgroundColor(Color.rgb(255, 255, 80));
+        tts.speak(caption, TextToSpeech.QUEUE_ADD, null,null);
+        btnCamera.setEnabled(true);
+        btnSelect.setEnabled(true);
+    }
+
+    /**
+     * Create a file Uri for saving an image or video
+     */
+    public File getPhotoFile() {
+        File filesDir = getFilesDir();
+        return new File(filesDir, fileName);
+    }
+
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        captioner.close();
+        lstm_captioner.close();
+    }
+}
